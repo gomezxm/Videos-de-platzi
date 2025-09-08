cacb086d306383f0769e062a3c4649d6b42d0403
gomezxm <miguel.gomez1@utp.ac.pa>
2025-09-08 10:51:47 -0500

    Se sube le primer programa que se realizo en el video de platzi

---
+++ Videos 1-10/Datos/Datos/Program.cs
@@ -0,0 +1,15 @@
+﻿//Variables con la que se manejara la data de entrada y el resultado
+int sideA;
+int sideB;
+int reslt;
+// le solicito al usario la data de entrada
+Console.WriteLine("Calcula el area de un triangulo");
+Console.WriteLine("Ingrese el valor del lado A:");
+sideA = Convert.ToInt32(Console.ReadLine()); // recibo la data de entrada
+
+Console.WriteLine("Ingrese el valor del lado B: ");
+sideB = Convert.ToInt32(Console.ReadLine()); // recibo la data de entrada
+
+reslt = sideA * sideB; //realizo la operacion
+//imprimo a consola la respuesta
+Console.WriteLine("El lado A es: " + sideA + ", el lado B:" + sideB + " El resultado es: " + reslt);
