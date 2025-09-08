b8f32184ea9fed8133385d1fd710a9f16d4b1cfa
gomezxm <miguel.gomez1@utp.ac.pa>
2025-09-08 11:04:42 -0500

    Hemos realizado un programa que calcula el area de un circulo, aqui hemos utlizado constantes para el manejo de operaciones

---
+++ Videos 1-10/Calc_Area_Circulo/Calc_Area_Circulo/Program.cs
@@ -0,0 +1,13 @@
+﻿//Programa para calcular el area de un circulo utilizando constantes
+
+Console.WriteLine("Bienvenido, calculemos circulos");
+//Pi * radio*radio(Formula para calcular el area)
+const double Pi = 3.1415;//creamos la constante que almacenara el valor de Pi
+
+//Solicitamos al usuario la data de entradas, que en este caso es el valor del radio
+Console.WriteLine("Ingrese el valor del radio:");
+double radio = Convert.ToDouble(Console.ReadLine());//declaramos la variable justo donde se va ingresar el dato(mala practica)
+
+double result = Pi * radio * radio;//Operacion realizada para obtener el area del circulo
+//se imprime a consola el resultado obtenido
+Console.WriteLine("El area de tu circulo es de: " + result);
