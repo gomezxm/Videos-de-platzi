16f73817292ac4a39b9dc45fad7141b9a2963936
gomezxm <miguel.gomez1@utp.ac.pa>
2025-09-10 11:16:58 -0500

    se ha subido el programa de C# de los operadores logicos

---
+++ Videos 11-13/Operadores Logicos/Operadores Logicos/Program.cs
@@ -0,0 +1,13 @@
+﻿//se declaran las varibales booleanas a utilizar
+bool value1 = true;
+bool value2 = true;
+bool value3 = false;
+
+bool resultAnd = value1 && value2 && value3;//esta variable recibira el 
+Console.WriteLine(resultAnd);
+
+bool resultOr= value1 || value2 || value3;
+Console.WriteLine(resultOr);
+
+bool resultAndOr = (value1 && value2) || value3;
+Console.WriteLine(resultAndOr);
