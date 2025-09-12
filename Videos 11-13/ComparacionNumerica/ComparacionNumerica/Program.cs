5e6b1d1f696f564164de13a0fe82712f73873e36
gomezxm <miguel.gomez1@utp.ac.pa>
2025-09-12 15:41:16 -0500

    Se subio un programa donde se manejan los operadores de comparacion numerica

---
+++ Videos 11-13/ComparacionNumerica/ComparacionNumerica/Program.cs
@@ -0,0 +1,31 @@
+﻿//variables a utilizar 
+var (value1, value2, value3) = (10, 1, 2);
+Console.WriteLine("Los valores son: "+value1+" "+value2 + " " + value3);
+
+/* 
+ Operadores de comparacion 
+== igual a 
+!= diferente a 
+> mayor que 
+< menor que 
+>= mayor o igual 
+<+ menor o igual 
+ */
+// como se ven los resultados de las distintas maneras de comparar
+bool result1 = value1 == value2;
+Console.WriteLine("El resultado de la compración value1 == value2: "+ result1);
+
+bool result2 = value1 != value2;
+Console.WriteLine("El resultado de la compración value1 != value2: " + result2);
+
+bool result3 = value1 > value2;
+Console.WriteLine("El resultado de la compración value1 > value2: " + result3);
+
+bool result4 = value1 < value2;
+Console.WriteLine("El resultado de la compración value1 < value2: " + result4);
+
+bool result5 = value1 >= value2;
+Console.WriteLine("El resultado de la compración value1 >= value2: " + result3);
+
+bool result6 = value1 <= value2;
+Console.WriteLine("El resultado de la compración value1 <= value2: " + result4);
